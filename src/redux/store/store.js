5847e5af24adb68dafdf295a65ede3255ab0ebba
Deep-Ramani <deepramani8505@gmail.com>
2022-03-25 16:19:29 +0530

    feat:Add middleware in Store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,5 +1,8 @@
 import { createStore } from "redux";
+import { applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "../reducers/reducer";
-const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(reducer,composeWithDevTools( applyMiddleware(thunk)))
 
 export default store;

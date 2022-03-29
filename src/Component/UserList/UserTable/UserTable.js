deff0109b42a0f6f8f57574eda34c947d811cc06
Deep-Ramani <deepramani8505@gmail.com>
2022-03-29 14:42:13 +0530

    feat: Add loader and pagination

--- src/Component/UserList/UserTable/UserTable.js
+++ src/Component/UserList/UserTable/UserTable.js
@@ -1,27 +1,29 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { CardHover } from "../../CardHover/CardHover";
 import { User } from "../User/User";
 import "./UserTable.css";
 import { FetchUser } from "../../../redux/actions/action";
-import Pagination from "../../Pagination/Pagination"
-// import { setUsers } from "../../../redux/actions/action";  
+import Pagination from "../../Pagination/Pagination";
+// import { setUsers } from "../../../redux/actions/action";
 
 const UserTable = (props) => {
-  const users = useSelector((state) => state.users);
+  const { users, loading, error, pageUpdated_data } = useSelector(
+    (state) => state
+  );
   const Dispatch = useDispatch();
   const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(()=>{
-    Dispatch(FetchUser(currentPage));
+  useEffect(() => {
+    Dispatch(FetchUser(currentPage, pageUpdated_data));
     console.log(currentPage);
-  },[currentPage]);
-  console.log("users: ",users);
+  }, [currentPage]);
+  console.log("users: ", users);
 
-  const paginate = (pageNumber) =>{
+  const paginate = (pageNumber) => {
     const current = pageNumber;
     setCurrentPage(current);
-  }
+  };
 
   // const fetchuser = async () => {
   //   const response = await axios
@@ -51,7 +53,6 @@ const UserTable = (props) => {
   //   fetchuser();
   // }, []);
 
-
   return (
     <>
       <div className="col-12">
@@ -65,14 +66,23 @@ const UserTable = (props) => {
               </tr>
             </thead>
             <tbody>
-              {users.loading?(
-                <h2>loading...</h2>
-              ): users.error ? (
-                <h2>{users.error}</h2>
-              ) :
-              (users.map((user, index) => (
-                <User key={index} user={user} index={index} />
-              ))
+              {loading && !error ? (
+                // <h2>loading...</h2>
+                <tr>
+                  <td colSpan="3" className="loader">
+                    <div className="snippet" data-title=".dot-spin">
+                      <div className="stage">
+                        <div className="dot-spin"></div>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              ) : error ? (
+                <h2>{error}</h2>
+              ) : (
+                users.map((user, index) => (
+                  <User key={index} user={user} index={index} />
+                ))
               )}
             </tbody>
           </table>
@@ -84,7 +94,7 @@ const UserTable = (props) => {
       </div>
 
       <div>
-        <Pagination paginate={paginate} currentPage={currentPage}/>
+        <Pagination paginate={paginate} currentPage={currentPage} />
       </div>
     </>
   );
